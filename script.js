c949391aa2d399e0b149e2f5fb250b7f4ce20b4e
malo-emonnet <166211212+malo-emonnet@users.noreply.github.com>
2024-04-12 11:56:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('acceptBtn').addEventListener('click', function() {
+    document.getElementById('welcomeBanner').style.display = 'none';
+});
+var acceptBtn = document.getElementById('acceptBtn');
+
+
+acceptBtn.addEventListener('click', function() {
+   
+    var welcomeBanner = document.getElementById('welcomeBanner');
+    
+   
+    welcomeBanner.style.display = 'none';
+});
